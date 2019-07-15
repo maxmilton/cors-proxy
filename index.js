723ccc67e27a1441d07e6f0d12227a6d9b4ca0ba
Max Milton <max@wearegenki.com>
2019-07-16 09:30:48 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers, no-console */
+
+'use strict';
+
+const http = require('http');
+const { send } = require('httpie');
+
+const PORT = process.env.PORT || 8081;
+
+/**
+ * @param {http.IncomingMessage} req - Client request.
+ * @param {http.ServerResponse} res - Server response.
+ */
+function requestHandler(req, res) {
+  // Strip leading `/`
+  const url = req.url.substring(1);
+
+  send(req.method, url)
+    .then((result) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+      );
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      res.setHeader('Content-Type', 'application/json');
+      res.end(result.data);
+    })
+    .catch((err) => {
+      res.statusCode = err.statusCode || 500;
+      res.end(err.message);
+    });
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(
+  PORT,
+  /** @param {Error} err - Node error. */
+  (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(`Server is listening on ${PORT}`);
+  },
+);
