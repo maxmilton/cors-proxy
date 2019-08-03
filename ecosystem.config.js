c4a0b8b166481ec7e04c650efccc8193731faba5
Max Milton <max@wearegenki.com>
2019-08-04 09:19:50 +1000

    Add sample pm2 config + scripts

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  apps: [
+    {
+      name: 'cors-proxy',
+      script: './index.js',
+
+      // eslint-disable-next-line sort-keys
+      env: {
+        NODE_ENV: 'production',
+        // SENTRY_DEBUG: true,
+        SENTRY_DSN: '__PUT_YOUR_DSN_HERE',
+      },
+    },
+  ],
+};
