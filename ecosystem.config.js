514bae91b0ac82066255a6c165d619c44ab936c8
Max Milton <max@wearegenki.com>
2019-12-16 11:42:45 +1100

    Clean up env config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,15 +3,11 @@
 module.exports = {
   apps: [
     {
-      name: 'cors-proxy',
-      script: './index.js',
-
-      // eslint-disable-next-line sort-keys
       env: {
         NODE_ENV: 'production',
-        // SENTRY_DEBUG: true,
-        SENTRY_DSN: '__PUT_YOUR_DSN_HERE__',
       },
+      name: 'cors-proxy',
+      script: './index.js',
     },
   ],
 };
